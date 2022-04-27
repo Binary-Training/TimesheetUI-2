0a51f00c1aba7b6f0c1e678f40d492f21d7f5b36
zeeshanbinary <zeeshan.ansari@binaryinformatics.com>
2022-04-27 17:16:40 +0530

    second commit

---
+++ src/layouts/tables/data/authorsTableData.js
@@ -0,0 +1,78 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDAvatar from "components/MDAvatar";
+import MDBadge from "components/MDBadge";
+
+// Images
+
+export default function data() {
+  const Author = ({ name }) => (
+    <MDBox display="flex" alignItems="center" lineHeight={1}>
+      <MDAvatar name={name} size="sm" />
+      <MDBox ml={2} lineHeight={1}>
+        <MDTypography display="block" variant="button" fontWeight="medium">
+          {name}
+        </MDTypography>
+      </MDBox>
+    </MDBox>
+  );
+
+  const Job = ({ title, description }) => (
+    <MDBox lineHeight={1} textAlign="left">
+      <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
+        {title}
+      </MDTypography>
+      <MDTypography variant="caption">{description}</MDTypography>
+    </MDBox>
+  );
+
+  return {
+    columns: [
+      { Header: "Project Name", accessor: "author", width: "15%", align: "center" },
+      { Header: "Project Manager Name", accessor: "function", align: "center" },
+      { Header: "Project Country", accessor: "status", align: "center" },
+      { Header: "Project Description", accessor: "employed", align: "center" },
+      { Header: "Approved Hour(s)", accessor: "action", align: "center" },
+    ],
+
+    rows: [
+      {
+        author: <Author name="3MRCR-301012" />,
+        function: <Job description="Feb5 Pm1" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        employed: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            23/04/18
+          </MDTypography>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+    ],
+  };
+}
